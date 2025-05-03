1ea6447434f8d199635f4f7076b55ccf6dbfcd81
JulioCezarbc <julio04.cordeiro@gmail.com>
2025-05-03 17:34:14 -0300

    Initial commit: Estrutura do projeto

---
+++ src/main/java/com/unasp/Prensadinho/PrensadinhoApplication.java
@@ -0,0 +1,13 @@
+package com.unasp.Prensadinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrensadinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrensadinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unasp/Prensadinho/PrensadinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unasp.Prensadinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrensadinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
