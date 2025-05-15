d2a5ed3145d00d59a148bfa02463efbf260d96e5
JulioCezarbc <julio04.cordeiro@gmail.com>
2025-05-15 20:29:47 -0300

    feat: adiciona repositório SpunRepository para acesso a clientes (Spun)

--- src/main/java/com/unasp/Prensadinho/repository/SpunRepository.java
+++ src/main/java/com/unasp/Prensadinho/repository/SpunRepository.java
@@ -1,2 +1,7 @@
-package com.unasp.Prensadinho.repository;public interface SpunRepository {
+package com.unasp.Prensadinho.repository;
+
+import com.unasp.Prensadinho.domain.Spun;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpunRepository extends JpaRepository<Spun, Long> {
 }
