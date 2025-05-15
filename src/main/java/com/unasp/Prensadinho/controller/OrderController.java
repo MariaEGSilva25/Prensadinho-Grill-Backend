99ca4c548953c73fe9c521626681020d5d789698
JulioCezarbc <julio04.cordeiro@gmail.com>
2025-05-15 20:29:44 -0300

    feat: adiciona OrderController com endpoint para criação de pedidos

--- src/main/java/com/unasp/Prensadinho/controller/OrderController.java
+++ src/main/java/com/unasp/Prensadinho/controller/OrderController.java
@@ -1,2 +1,48 @@
-package com.unasp.Prensadinho.controller;public class OrderService {
+package com.unasp.Prensadinho.controller;
+
+import com.unasp.Prensadinho.DTO.orderDTO.OrderDTO;
+import com.unasp.Prensadinho.DTO.productDTO.ProductDTO;
+import com.unasp.Prensadinho.domain.Order;
+import com.unasp.Prensadinho.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody OrderDTO order) {
+        service.createOrder(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Void> updateOrder(@PathVariable Long id ,@RequestBody OrderDTO order){
+//        service.updateOrder(id, order);
+//        return ResponseEntity.noContent().build();
+//    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id){
+        service.deleteOrder(id);
+        return ResponseEntity.noContent().build();
+    }
 }
