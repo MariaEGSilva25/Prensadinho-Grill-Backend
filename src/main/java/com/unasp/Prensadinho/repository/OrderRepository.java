4e94191c84fafbf912bb2540ba4b146e71451ca8
JulioCezarbc <julio04.cordeiro@gmail.com>
2025-05-15 20:29:47 -0300

    feat: adiciona repositório OrderRepository para acesso aos pedidos

--- src/main/java/com/unasp/Prensadinho/repository/OrderRepository.java
+++ src/main/java/com/unasp/Prensadinho/repository/OrderRepository.java
@@ -1,2 +1,7 @@
-package com.unasp.Prensadinho.repository;public interface OrderRepository {
+package com.unasp.Prensadinho.repository;
+
+import com.unasp.Prensadinho.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
